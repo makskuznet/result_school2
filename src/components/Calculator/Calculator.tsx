c464d7e1b24e9e925ceeec407389ed0447c08962
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:59:55 +0300

    fix: merge conflicts

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,9 +1,21 @@
+<<<<<<< HEAD
 import {Button, Col, Layout, Row} from 'antd';
+=======
+import {Button, Col, Input, Layout, Row, Typography} from 'antd';
+import {useState} from 'react';
+>>>>>>> b96a0d1 (add history)
 
 export const Calculator = () => {
   const numberActions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const mathActions: string[] = ['+', '-', '*', '/', '%'];
 
+  const [historyList, setHistoryList] = useState<string[]>([]);
+
+  const addHistoryItem = (historyItem: string) => setHistoryList([...historyList, historyItem]);
+
+  const goAction = (action: string | number) => {
+    addHistoryItem(`${action}`);
+  }
 
   return (
     <Layout style={{
@@ -11,12 +23,26 @@ export const Calculator = () => {
       margin: '0 auto',
     }}>
       <Layout.Content>
+<<<<<<< HEAD
         <Row>
+=======
+        <Row gutter={[0, 8]}>
+          <Col span={24}>
+            <Typography>
+              <p>
+                {historyList.join(' ')}
+              </p>
+            </Typography>
+          </Col>
+          <Col span={24}>
+            <Input size={'large'} value={3} disabled />
+          </Col>
+>>>>>>> b96a0d1 (add history)
           <Col span={20}>
             <Row gutter={[8, 8]}>
               {numberActions.map(n => (
                 <Col span={7} key={n}>
-                  <Button shape={'circle'} size={'large'}>{n}</Button>
+                  <Button onClick={() => goAction(n)} shape={'circle'} size={'large'}>{n}</Button>
                 </Col>
               ))}
             </Row>
@@ -25,7 +51,7 @@ export const Calculator = () => {
             <Row gutter={[8, 8]}>
               {mathActions.map(n => (
                 <Col span={24} key={n}>
-                  <Button shape={'circle'} size={'large'}>{n}</Button>
+                  <Button onClick={() => goAction(n)} shape={'circle'} size={'large'}>{n}</Button>
                 </Col>
               ))}
             </Row>
